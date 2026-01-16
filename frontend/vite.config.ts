d9c4b3730025e48014846171606692af289a9c51
stuparic <stuparic@hotmail.com>
2026-01-16 19:00:13 +0100

    icon fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,26 +20,26 @@ export default defineConfig({
         display: 'standalone',
         orientation: 'portrait',
         scope: '/',
-        start_url: '/',
+        start_url: '/?v=2',
         icons: [
           {
-            src: 'pwa-64x64.png',
+            src: 'pwa-64x64.png?v=2',
             sizes: '64x64',
             type: 'image/png'
           },
           {
-            src: 'pwa-192x192.png',
+            src: 'pwa-192x192.png?v=2',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'pwa-512x512.png?v=2',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'maskable-icon-512x512.png',
+            src: 'maskable-icon-512x512.png?v=2',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
